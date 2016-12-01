813f70ed23a37b0897b1f8f9413a17eb8566107e
Riz Rudra <rizwanulrudra@gmail.com>
2016-12-01 16:25:44 +0700

    Steam dropdown angularjs
    Angularjs add/remove dropdown/textfield function

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+
+var app = angular.module('RizApp', []);
+
+  app.controller('MainCtrl', function($scope) {
+
+  $scope.choices = [{id: 'choice1'}, {id: 'choice2'}];
+  
+  $scope.addNewChoice = function() {
+    var newItemNo = $scope.choices.length;
+    $scope.choices.push({'id':'choice'+newItemNo});
+  };
+    
+  $scope.removeChoice = function() {
+    var lastItem = $scope.choices.length-1;
+    $scope.choices.splice(lastItem);
+  };
+  
+});
