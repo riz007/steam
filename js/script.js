e005a9f39e15640acb068ac5679917db91d9903e
Riz Rudra <rizwanulrudra@gmail.com>
2016-12-01 23:18:23 +0700

    Delete functionality updated

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@
 
 var app = angular.module('RizApp', []);
 
-  app.controller('MainCtrl', function($scope) {
 
-  $scope.choices = [{id: 'choice1'}, {id: 'choice2'}];
+app.controller('rizCtrl', function($scope) {
+
+    $scope.choices = [];
   
-  $scope.addNewChoice = function() {
-    var newItemNo = $scope.choices.length;
-    $scope.choices.push({'id':'choice'+newItemNo});
-  };
+    $scope.addNewChoice = function() {
+
     
-  $scope.removeChoice = function() {
-    var lastItem = $scope.choices.length-1;
-    $scope.choices.splice(lastItem);
-  };
-  
+    	
+    	var newItemNo = $scope.choices.length;
+        $scope.choices.push({'id':'choice'});
+
+    
+      
+
+    };
+
+    $scope.removeChoice = function(choice) {
+
+
+   var index = $scope.choices.indexOf(choice);
+   $scope.choices.splice(index,1);
+
+
+    };
+
 });
